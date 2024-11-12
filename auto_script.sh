6a48d4d9c2657aab7f887842067e18dbe118a6a6
toto <jules.meignal@epitech.eu>
2024-11-12 15:41:47 +0100

    feat: reworked autoscript

--- auto_script.sh
+++ auto_script.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/bash
 
-REF=$1
-INPUT=$2
-OUTPUT=$3
+PROJ_LOC="/usr/Vop2x/"
+REF_FILE=$1
+OUT_FOLD=$2
+ZIP_FILE=$(date --date='-1 month' +"%Y-%m.zip" | tr -d '\n')
+ZIP_FOLD="$PROJ_LOC/zip/"
+PASS=""
+USER=""
+IP=""
 
 print_error () {
     echo "Error: $1"
@@ -16,6 +21,7 @@ is_file_valid () {
     fi
 }
 
+# Help
 if [ "$1" == "-h" ]; then
     echo "Usage:"
     echo "      automakefile reference input [output]"
@@ -29,28 +35,30 @@ if [ "$1" == "-h" ]; then
     echo "      input : zip file extracted from VOP containing data to be treated"
     echo "          ex : zip/2024-03.zip"
     echo
-    echo "      output : output directory, will default to \"out\""
+    echo "      output : output directory, will default to \"/media/bigpart/data/CDR/files/CRD-Client\""
     exit 0
 fi
 
-if [ -z $REF ]; then
+# Error handling
+if [ -z $REF_FILE ]; then
     print_error "missing argument : reference_file"
     exit 2
-elif [ -z $INPUT ]; then
+elif [ -z $ZIP_FOLD ]; then
     print_error "missing argument : input"
     exit 2
 fi
 
-is_file_valid $REF ".csv"
+is_file_valid $REF_FILE ".csv"
 
-FILE=$(echo "$INPUT/$(date --date='-1 month' +"%Y-%m.zip")")
-FOLDER=$(echo "$INPUT/$(date --date='-1 month' +"%Y-%m")")
+# Récupère le zip via un protocole ftp, peut être rendu plus sécurisé
+wget "ftp://${USER}:${PASS}@${IP}/$ZIP_FILE" -P $ZIP_FOLD
 
-if [ -z $OUTPUT ]; then
-    OUTPUT="out_bash"
-    # OUTPUT="/media/bigpart/data/CDR/files/CRD-Client"
+# Fichier output pour clients, définissable pour débug
+if [ -z $OUT_FOLD ]; then
+    OUT_FOLD="/media/bigpart/data/CDR/files/CRD-Client"
 fi
 
-# ajouter chemin absolu (/usr/X/vop2pdf)
-./vop2pdf.py $REF $FILE -o $OUTPUT
-./vop2csv.py $REF $FILE -o $OUTPUT
+# Commandes Vop2x (adaptables)
+$PROJ_LOC/vop2pdf.py $REF_FILE $ZIP_FOLD$ZIP_FILE -d -o $OUT_FOLD
+$PROJ_LOC/vop2csv.py $REF_FILE $ZIP_FOLD$ZIP_FILE -d -o $OUT_FOLD
+
